8df0fcf06ed042147a4876a19bb0fa8f7b70f9c9
Susie ONeil <susie@oneil.com.au>
2024-03-15 11:16:05 +0800

    Add documentation to board.py

--- src/board.py
+++ src/board.py
@@ -18,26 +18,59 @@ class PositionOccupiedError(BoardError):
 
 class Board:
     def __init__(self, size=3) -> None:
+        """
+        Initializes a new instance of the Board class.
+
+        Args:
+            size (int, optional): The size of the board. Defaults to 3.
+        """
         self.size = size
         self.grid = self._create_2d_grid()
 
     def _create_2d_grid(self):
+        """
+        Creates a 2-dimensional square grid of the board.
+
+        Returns:
+            list: Square 2d list of None values
+        """
         row = [None] * self.size
         return [row.copy() for _ in range(self.size)]
 
     def is_full(self) -> bool:
+        """
+        Checks if the all positions on the board are filled.
+
+        Returns:
+            bool: False if any None values, True otherwise
+        """
         for row in self.grid:
             if None in row:
                 return False
         return True
 
     def is_position_occupied(self, row, col) -> bool:
+        """
+        Checks if board position already has a player
+
+        Args:
+            row (int): the index of a row in the grid
+            col (int): the index of an item in the row
+
+        Returns:
+            bool: False if no player, True otherwise
+        """
         return self.grid[row][col] is not None
 
     def make_move(self, row, col, player) -> None:
-        # Fixed the logic not raising the error (see source: Check Range Separately)
-        # if row < 0 or row >= self.size or col < 0 or col >= self.size:
-        # Using range() might improve readability.
+        """
+        Checks a move is valid and adds player to board position
+
+        Args:
+            row (int): index of the row
+            col (int): index of the column
+            player (Player): the current player
+        """
         if row not in range(0, self.size) or col not in range(0, self.size):
             raise InvalidPositionError("Invalid position")
         if self.is_position_occupied(row, col):
@@ -46,15 +79,33 @@ class Board:
         self.grid[row][col] = player
 
     def get_winner(self) -> None | Player:
+        """
+        Checks if the board contains a winning player.
+
+        Returns:
+             None or Player: Winner if any, None otherwise.
+        """
         return self._has_horizontal_winner() or self._has_vertical_winner() or self._has_diagonal_winner()
 
     def _has_horizontal_winner(self) -> None | Player:
+        """
+        Checks each row in the board for a winning player.
+
+        Returns:
+             None or Player: Winner if any, None otherwise.
+        """
         for row in self.grid:
             player = row[0]
             if player and all(players == player for players in row):
                 return player
 
     def _has_vertical_winner(self) -> None | Player:
+        """
+        Checks each column in the grid for a winning player.
+
+        Returns:
+             None or Player: Winner if any, None otherwise.
+        """
         for col, player in enumerate(self.grid[0]):
             if not player:
                 continue
@@ -65,6 +116,12 @@ class Board:
                 return player
 
     def _has_diagonal_winner(self) -> None | Player:
+        """
+        Checks the right and left diagonals in the grid for a winning player.
+
+        Returns:
+             None or Player: Winner if any, None otherwise.
+        """
         # check player in right diagonal
         player_right = self.grid[0][0]
         if player_right:
@@ -88,9 +145,11 @@ class Board:
                 return player_left
 
     def __str__(self) -> str:
+        """
+        Returns a string representation of the board.
+
+        Returns:
+            str: The string representation of the board.
+        """
         return "\n".join([str([str(col) if col else ' ' for col in row])
                           for row in self.grid])
-
-
-if __name__ == '__main__':
-    pass
